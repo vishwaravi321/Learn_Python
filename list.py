d02bf1f56f78810b9c87b1dd05741eb8f03a78d1
vishwa <vishwaravi321@gmail.com>
2022-11-08 12:00:39 +0530

    First learning step [08/11/22]

---
+++ list.py
@@ -0,0 +1,40 @@
+print("Lists are mutable objects which means it can be manipulatd after creation")
+
+
+nums = []
+
+print(nums)
+
+print("adding elements to the list")
+nums.append(21)
+print(nums)
+
+nums.append(2.11)
+print(nums)
+
+nums.append("Hey")
+print(nums)
+
+print("Printing specific elements from the string")
+
+print("First element of the list:")
+print(nums[0])  
+
+print("Second element of the list:")
+print(nums[1])
+
+print("And so on....!!!")
+
+
+
+print("deleting elements from the string")
+print(nums)
+nums.pop()
+print(nums)
+
+nums.pop()
+print(nums)
+
+nums.pop()
+print(nums)
+
---
+++ tuple.py
@@ -0,0 +1,7 @@
+print("Tuple is an immutable object")
+
+
+
+tup = (1,'a', 1.2 )
+
+print(tup)
