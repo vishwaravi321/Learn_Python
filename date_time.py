25d726af63f08a1ec65483356c242b8c2b394be9
vishwa <nxweb@INBOOK_X1_SLIM.localdomain>
2022-12-08 17:41:41 +0530

    First problem at nxweb

--- date_time.py
+++ date_time.py
@@ -1,27 +1,22 @@
 import datetime
 
-
-
 def compareDates(curDate, postDate, postTime):
-
-    noon = datetime.time(18,0,0)
-
+    noon = datetime.time(12,0,0)
     if (curDate ==  postDate) and (postTime <= noon):
         print('Allow')
     else:
         print("Don't allow")
 
+
 def dateConvertion(strDate):
-    date = datetime.datetime.strptime(strDate, '%m-%d-%Y').date() 
-    print(date)
+    date = datetime.datetime.strptime(strDate, '%d-%m-%Y').date() 
     return date
 
-
 date = datetime.datetime.now().date()
+
 time = datetime.datetime.now().time()
 
-
-conDate = dateConvertion('12-8-2022')
+conDate = dateConvertion(input("Enter Date in 'dd-mm-2022':"))
 
 compareDates(date,conDate,time,)
 
